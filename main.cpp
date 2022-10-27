89a4c79a8f81ea7257fd30cd25542d0f9b18dc12
TÜRKAY BİLİYOR <turkaybiliyor@hotmail.com>
2022-10-27 14:12:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -49,6 +49,12 @@
 
 //--------------------------------------------------------------------------->
 
+// public, protected, and private inheritance have the following features:
+
+// public inheritance makes public members of the base class public in the derived class, and the protected members of the base class remain protected in the derived class.
+// protected inheritance makes the public and protected members of the base class protected in the derived class.
+// private inheritance makes the public and protected members of the base class private in the derived class.
+
 //içinde en az bir pure(saf) metod olan class
 class AbstractClass //Abtstract Class
 {
